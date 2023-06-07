09549f4dca5283c4e1defae2d82943014629c6a5
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-06-07 15:01:46 +0300

    update js, add new js files

---
+++ src/js/fetchImages.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+const API_KEY = '37056848-912ded0eb5e75838ece32e5ab';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+//нас завжди (майже 100%) буде цікавити data з промісу, що повертається, тому деструктуризуємо
+async function fetchImages(q, page) {
+  try {
+    const { data } = await axios({
+      params: {
+        key: API_KEY,
+        q,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: 40,
+      },
+    });
+    return data;
+  } catch (error) {
+    Notiflix.Notify.failure(
+      `Oooops... Something goes wrong. Please, try again.`
+    );
+  }
+}
+
+export { fetchImages };
---
+++ src/js/renderGallery.js
@@ -0,0 +1,40 @@
+// const galleryContainer = document.querySelector('.gallery');
+import { galleryContainer } from './index.js';
+function renderGallery(photos) {
+  const markup = photos
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a class="link-card" href="${largeImageURL}">
+                  <div class="photo-card">
+                  <img src='${webformatURL}' alt='${tags}' loading="lazy" />
+                  <div class="info">
+                    <p class="info-item">
+                      <b>Likes</b>${likes}
+                    </p>
+                    <p class="info-item">
+                      <b>Views</b>${views}
+                    </p>
+                    <p class="info-item">
+                      <b>Comments</b>${comments}
+                    </p>
+                    <p class="info-item">
+                      <b>Downloads</b>${downloads}
+                    </p>
+                  </div>
+                </div>
+                </a>`;
+      }
+    )
+    .join('');
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+export { renderGallery };
