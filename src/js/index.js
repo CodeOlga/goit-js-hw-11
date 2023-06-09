a6b1c1a66a2a3b1c989d48435b35c6be632150f6
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-06-09 12:35:54 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -85,25 +85,32 @@ async function getImages(query, page) {
 }
 
 function handleSubmit(e) {
-  e.preventDefault();
-  const inputValue = e.target.elements.searchQuery.value;
+  try {
+    e.preventDefault();
+    const inputValue = e.target.elements.searchQuery.value;
 
-  //якщо inputValue - це порожній рядок; або те значення, що зараз в inputValue - це глобальна змінни,
-  //за якою йде пошук - то виходимо з функції (щоб не слати запити на сервер при порожньому інпуті,
-  //або повторно декілька запитів за одним й тим же словом (queryToFetch))
-  if (!inputValue.trim() || inputValue === queryToFetch) {
-    return;
+    //якщо inputValue - це порожній рядок; або те значення, що зараз в inputValue - це глобальна змінни,
+    //за якою йде пошук - то виходимо з функції (щоб не слати запити на сервер при порожньому інпуті,
+    //або повторно декілька запитів за одним й тим же словом (queryToFetch))
+    if (!inputValue.trim() || inputValue === queryToFetch) {
+      return;
+    }
+
+    queryToFetch = inputValue;
+    pageToFetch = 1;
+    galleryContainer.innerHTML = '';
+
+    //знимаємо старий observer при новому запиті
+    observer.unobserve(guard);
+
+    getImages(queryToFetch, pageToFetch);
+    searchForm.reset();
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure(
+      `Ooops... Something goes wrong. Please, try again.`
+    );
   }
-
-  queryToFetch = inputValue;
-  pageToFetch = 1;
-  galleryContainer.innerHTML = '';
-
-  //знимаємо старий observer при новому запиті
-  observer.unobserve(guard);
-
-  getImages(queryToFetch, pageToFetch);
-  searchForm.reset();
 }
 
 export { galleryContainer };
